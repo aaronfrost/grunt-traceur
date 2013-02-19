736d13478e6934652d658f89c11d60152361c45a
Aaron Frost <aaron@hfs.(none)>
2013-02-19 16:43:33 -0700

    remove build dir

--- build/js/file2.js
+++
@@ -1,9 +0,0 @@
-var $__toObject = function(value) {
-  if (value == null) throw TypeError();
-  return Object(value);
-};
-var vals = [1, 2, 3, 4];
-addFour.apply(null, $__toObject(vals));
-function addFour(a, b, c, d) {
-  console.log(a + b + c + d);
-}
