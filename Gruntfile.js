900ee7da9883e0f1a9d90f4630322e958be46bb8
Aaron Frost <aaron@hfs.(none)>
2013-02-19 16:41:44 -0700

    some changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,22 +12,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    // jshint: {
-    //   all: [
-    //     'Gruntfile.js',
-    //     'tasks/*.js',
-    //     '<%= nodeunit.tests %>',
-    //   ],
-    //   options: {
-    //     jshintrc: '.jshintrc',
-    //   },
-    // },
-
-    // // Before generating any new files, remove any previously-created files.
-    // clean: {
-    //   tests: ['tmp'],
-    // },
-
+    
     // Configuration to be run (and then tested).
     traceur: {
       custom: {
@@ -37,26 +22,11 @@ module.exports = function(grunt) {
       },
     },
 
-    // // Unit tests.
-    // nodeunit: {
-    //   tests: ['test/*_test.js'],
-    // },
-
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-clean');
-  // grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  // grunt.registerTask('test', ['clean', 'traceur', 'nodeunit']);
-
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['traceur']);
 
 };
